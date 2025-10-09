cae85fccc38c5293fdedf339135a73b256508778
Ayandip paul <152761756+Ayandip13@users.noreply.github.com>
2025-10-09 23:52:33 +0530

    fetch data from API using axios and display in FlatList; add .env for API URL

--- App.tsx
+++ App.tsx
@@ -1,11 +1,33 @@
+import axios from 'axios';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import { FlatList, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [data, setData] = useState(null);
+  const getTheData = async () => {
+    try {
+      const response = await axios.get(process.env.EXPO_PUBLIC_API_URL!);
+      setData(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  useEffect(()=>{
+    getTheData();
+  },[])
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
+      <FlatList
+      data={data?.data}
+      keyExtractor={(item) => item.id.toString()}
+      renderItem={({ item }) => {
+        return (
+          <Text>{item.title}</Text>
+        )
+      }}
+      />
     </View>
   );
 }
