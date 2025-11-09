e1d6250c2f361a39e2f5b5dd078cfab9dcffcd10
Ayandip paul <152761756+Ayandip13@users.noreply.github.com>
2025-11-09 23:47:06 +0530

    add contextApi initialization

--- App.tsx
+++ App.tsx
@@ -5,6 +5,7 @@ import { Provider } from "react-redux";
 import store from "./redux/store";
 import { ModalPortal } from "react-native-modals";
 import { BackHandler } from "react-native";
+import { UserContext } from "./UserContext";
 
 // Temporary fix for react-native-modals BackHandler issue
 if (!BackHandler.removeEventListener) {
@@ -15,20 +16,22 @@ const App = () => {
   return (
     <>
       <Provider store={store}>
-        <StackNavigator />
-        <ModalPortal />
-        <ToastManager
-          position="top"
-          style={{
-            width: 350,
-            height: 35,
-            borderRadius: 10,
-            alignSelf: "center",
-          }}
-          duration={2000}
-          dismissOnPress={true}
-          swipeable={true}
-        />
+        <UserContext>
+          <StackNavigator />
+          <ModalPortal />
+          <ToastManager
+            position="top"
+            style={{
+              width: 350,
+              height: 35,
+              borderRadius: 10,
+              alignSelf: "center",
+            }}
+            duration={2000}
+            dismissOnPress={true}
+            swipeable={true}
+          />
+        </UserContext>
       </Provider>
     </>
   );
