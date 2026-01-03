fd93dc495fac37afcd0789a6e0ea1e8c2a3b8a62
Ayandip paul <152761756+Ayandip13@users.noreply.github.com>
2026-01-03 17:15:51 +0530

    Implement camera and gallary picker feature

--- screens/EditProfile.tsx
+++ screens/EditProfile.tsx
@@ -1,41 +1,201 @@
-import { Image, Text, TouchableOpacity, View } from "react-native";
-import React from "react";
-import { Entypo } from "@expo/vector-icons";
+import {
+  Image,
+  Text,
+  TouchableOpacity,
+  View,
+  Modal,
+  StyleSheet,
+} from "react-native";
+import React, { useState } from "react";
+import { Entypo, Ionicons, MaterialIcons } from "@expo/vector-icons";
+import * as ImagePicker from "expo-image-picker";
+
 const EditProfile = () => {
+  const [modalVisible, setModalVisible] = useState(false);
+  const [image, setImage] = useState<string | null>(null);
+
+  const pickFromGallery = async () => {
+    const permission = await ImagePicker.requestMediaLibraryPermissionsAsync();
+
+    if (!permission.granted) return;
+
+    const result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ["images"],
+      quality: 0.8,
+      allowsEditing: true,
+      aspect: [1, 1],
+    });
+
+    if (!result.canceled) {
+      setImage(result.assets[0].uri);
+    }
+    setModalVisible(false);
+  };
+
+  const pickFromCamera = async () => {
+    const permission = await ImagePicker.requestCameraPermissionsAsync();
+
+    if (!permission.granted) return;
+
+    const result = await ImagePicker.launchCameraAsync({
+      quality: 0.8,
+      allowsEditing: true,
+      aspect: [1, 1],
+      mediaTypes: ["images"],
+    });
+
+    if (!result.canceled) {
+      setImage(result.assets[0].uri);
+    }
+    setModalVisible(false);
+  };
+
   return (
     <View
       style={{ flex: 1, paddingHorizontal: 20, backgroundColor: "#f5f5f5" }}
     >
       <TouchableOpacity
-        activeOpacity={0.5}
         style={{ alignItems: "center", marginTop: 20 }}
-        onPress={() => console.log("Edit Profile pressed")}
+        onPress={() => setModalVisible(true)}
       >
         <Image
-          source={require("../assets/person.png")}
-          style={{ width: 150, height: 90, borderRadius: 50, marginTop: 20 }}
-          resizeMode="contain"
-          tintColor="#008e97"
+          source={image ? { uri: image } : require("../assets/person.png")}
+          style={{
+            width: 150,
+            height: 150,
+            borderRadius: 100,
+            marginTop: 20,
+          }}
         />
-        <Entypo
-          name="edit"
-          size={25}
-          color="#000"
+        <MaterialIcons
+          name="mode-edit"
+          size={28}
+          color="#999"
           style={{
             position: "absolute",
-            right: "35%",
-            top: "80%",
+            right: "30%",
+            bottom: 10,
           }}
         />
       </TouchableOpacity>
-      <View>
-        <Text style={{ fontSize: 18, fontWeight: "600", marginTop: 10 }}>
-          Edit Your Profile
-        </Text>
-        {/* Add form fields for editing profile information here */}
-      </View>
+
+      <Text style={{ fontSize: 18, fontWeight: "600", marginTop: 15 }}>
+        Edit Your Profile
+      </Text>
+
+      {/* Image Picker Modal */}
+      <Modal
+        transparent
+        animationType="slide"
+        visible={modalVisible}
+        onRequestClose={() => setModalVisible(false)}
+        onDismiss={() => setModalVisible(false)}
+        presentationStyle="overFullScreen"
+      >
+        <View
+          style={{
+            flex: 1,
+            backgroundColor: "rgba(0,0,0,0.12)",
+            justifyContent: "flex-end",
+          }}
+        >
+          <View
+            style={{
+              backgroundColor: "#fff",
+              height: "30%",
+              borderTopLeftRadius: 20,
+              borderTopRightRadius: 20,
+              justifyContent: "center",
+            }}
+          >
+            <Text
+              style={{
+                fontSize: 16,
+                fontWeight: "600",
+                textAlign: "center",
+              }}
+            >
+              Change Profile Photo
+            </Text>
+            <View
+              style={{
+                flexDirection: "row",
+                gap: 10,
+                justifyContent: "space-evenly",
+                marginTop: 30,
+              }}
+            >
+              <TouchableOpacity
+                style={{
+                  alignItems: "center",
+                  gap: 12,
+                }}
+                onPress={pickFromCamera}
+              >
+                <Ionicons name="camera" size={50} color="#008e97" />
+                <Text
+                  style={{
+                    fontSize: 15,
+                    fontWeight: "500",
+                  }}
+                >
+                  Take Photo
+                </Text>
+              </TouchableOpacity>
+
+              <TouchableOpacity
+                style={{
+                  alignItems: "center",
+                  gap: 12,
+                }}
+                onPress={pickFromGallery}
+              >
+                <Ionicons name="image" size={50} color="#008e97" />
+                <Text
+                  style={{
+                    fontSize: 15,
+                    fontWeight: "500",
+                    textAlign: "center",
+                  }}
+                >
+                  Choose from {"\n"} Gallery
+                </Text>
+              </TouchableOpacity>
+            </View>
+
+            <TouchableOpacity
+              style={[
+                {
+                  flexDirection: "row",
+                  alignItems: "center",
+                  gap: 12,
+                  paddingVertical: 14,
+                },
+                styles.cancel,
+              ]}
+              onPress={() => setModalVisible(false)}
+            >
+              <Text style={styles.cancelText}>Cancel</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </Modal>
     </View>
   );
 };
 
 export default EditProfile;
+const styles = StyleSheet.create({
+  cancel: {
+    justifyContent: "center",
+    borderTopWidth: 1,
+    borderColor: "#eee",
+    marginTop: 10,
+  },
+  cancelText: {
+    textAlign: "center",
+    color: "#ff3b30",
+    fontSize: 15,
+    fontWeight: "600",
+  },
+});
